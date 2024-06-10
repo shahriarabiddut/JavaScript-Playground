645de83c6cc9b2507cb4e22eaae0975259937f3a
Shahriar Ahmed <54743793+shahriarabiddut@users.noreply.github.com>
2024-06-10 16:03:36 +0600

    leetcode 1200. Minimum Absolute Difference.js

--- Array /leetcode 1200. Minimum Absolute Difference.js
+++ Array /leetcode 1200. Minimum Absolute Difference.js
@@ -1,1 +1,22 @@
-
+/**
+ * @param {number[]} arr
+ * @return {number[][]}
+ */
+var minimumAbsDifference = function(arr) {
+    arr.sort((a,b)=>a-b);
+    const result = new Array();
+    let min = Infinity;
+    
+    for(let i = 0;i< arr.length -1 ;i++){
+        let diff = arr[i+1] - arr[i];
+        min = Math.min(diff,min);
+    }
+    
+    for(let i = 0;i<arr.length-1 ;i++){
+        let diff = arr[i+1] - arr[i];
+        if(min===diff){
+            result.push([arr[i],arr[i+1]]);
+        }
+    }
+    return result;
+};
